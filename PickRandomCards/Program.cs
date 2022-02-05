95dfa1af15928721c3dfa4576a62410fefb3e47b
Mateusz <mateusz.jachym.44@outlook.com>
2022-02-05 18:09:33 +0100

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace PickRandomCards
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine("Enter a number of cartds to pick: ");
+			string line = Console.ReadLine();
+
+			if (int.TryParse(line, out int numberOfCards))
+			{
+				string[] chosenCards = CardPicker.PickSomeCards(numberOfCards);
+				WriteLine("\nThe cards are:");
+				foreach (string card in chosenCards)
+				{
+					WriteLine(card);
+				}
+				ReadKey();
+			}
+			else
+			{
+				WriteLine("Number was not valid! Run again");
+				ReadKey();
+			}
+		}
+	}
+}
